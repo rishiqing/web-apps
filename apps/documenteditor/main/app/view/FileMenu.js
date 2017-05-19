2ed1f4889990146ff23849cbad1bd53b950f159a
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-05-19 11:08:32 +0300

    [DE] Fix loading not-desktop app.

--- apps/documenteditor/main/app/view/FileMenu.js
+++ apps/documenteditor/main/app/view/FileMenu.js
@@ -288,7 +288,7 @@ define([
         setApi: function(api) {
             this.api = api;
             this.panels['info'].setApi(api);
-            this.panels['protect'].setApi(api);
+            if (this.panels['protect']) this.panels['protect'].setApi(api);
         },
 
         loadDocument: function(data) {
