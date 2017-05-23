e275f1132a5279fb911f30226f277cf6f5d4918b
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-05-23 15:02:28 +0300

    [DE] Sign visible signature.

--- apps/documenteditor/main/app/view/DocumentHolder.js
+++ apps/documenteditor/main/app/view/DocumentHolder.js
@@ -80,7 +80,6 @@ define([
             me.fastcoauthtips = [];
             me._currentMathObj = undefined;
             me._currentParaObjDisabled = false;
-            me._currentSignGuid = null;
 
             var showPopupMenu = function(menu, value, event, docElement, eOpts){
                 if (!_.isUndefined(menu)  && menu !== null && me.mode.isEdit){
@@ -699,25 +698,32 @@ define([
             };
 
             var onSignatureClick = function(guid) {
-                me._currentSignGuid = guid;
-                me.api.asc_GetDefaultCertificate();
-            };
-
-            var onDefaultCertificate = function(certificate) {
-                var win = new DE.Views.SignDialog({
-                        api: me.api,
-                        signType: 'visible',
-                        handler: function(dlg, result) {
-                            if (result == 'ok') {
-                                var props = dlg.getSettings();
-                                me.api.asc_Sign(props.certificateId, me._currentSignGuid, props.image);
-                            }
-                            Common.NotificationCenter.trigger('edit:complete');
+                if (_.isUndefined(me.fontStore)) {
+                    me.fontStore = new Common.Collections.Fonts();
+                    var fonts = DE.getController('Toolbar').getView('Toolbar').cmbFontName.store.toJSON();
+                    var arr = [];
+                    _.each(fonts, function(font, index){
+                        if (!font.cloneid) {
+                            arr.push(_.clone(font));
                         }
                     });
+                    me.fontStore.add(arr);
+                }
+
+                var win = new DE.Views.SignDialog({
+                    api: me.api,
+                    signType: 'visible',
+                    fontStore: me.fontStore,
+                    handler: function(dlg, result) {
+                        if (result == 'ok') {
+                            var props = dlg.getSettings();
+                            me.api.asc_Sign(props.certificateId, guid, props.image);
+                        }
+                        Common.NotificationCenter.trigger('edit:complete');
+                    }
+                });
 
                 win.show();
-                win.setSettings(certificate);
             };
 
             var onTextLanguage = function(langid) {
@@ -1525,7 +1531,6 @@ define([
                     this.api.asc_registerCallback('asc_onHideSpecialPasteOptions',      _.bind(onHideSpecialPasteOptions, this));
 
                     this.api.asc_registerCallback('asc_onSignatureClick',               _.bind(onSignatureClick, this));
-                    this.api.asc_registerCallback('on_signature_defaultcertificate_ret', _.bind(onDefaultCertificate, this));
                 }
 
                 return this;
